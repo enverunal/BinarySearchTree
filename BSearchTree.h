5c2837e9e8bf1dfb3b52664e6661bc14101ad97c
Enver Nuri Ünal <31821023+enverunal@users.noreply.github.com>
2021-03-06 20:20:26 +0300

    Add files via upload

--- BSearchTree.h
+++ BSearchTree.h
@@ -7,50 +7,143 @@
 
 
 template <typename t>
-struct bstNode
+class bstNode
 {
-	t m_value{};
-	bstNode<t>* right{};
+private:
+    t m_value{};
+    bstNode<t>* right{};
 	bstNode<t>* left{};
+    template <typename u> friend class BSearchTree;
+public:
+    t value() {
+        return m_value;
+    }
 };
 
 template <typename t>
 class BSearchTree {
 private:
 	bstNode<t>* start{};
-	int_fast64_t length{ 0 };
+	size_t length{ 0 };
+
     //traverse function does allow elements to change
     //could be made public if required or wrapped with a safer function
+    //preorder traverse
     void traverse(std::function<void(bstNode<t>* element)> process);
 public:
-    BSearchTree() {};
-    BSearchTree(std::initializer_list<t> initList);
-    BSearchTree(BSearchTree<t>& bst);
+    /*
+        *noexcept
+    */
+    BSearchTree() noexcept{};
+
+    /*
+        Imports each element in iterable sturcture to this tree
+        *noexcept
+    */
+    BSearchTree(std::initializer_list<t> initList) noexcept;
+
+    /*
+        Imports each element in iterable sturcture to this tree
+        *noexcept
+    */
+    BSearchTree(const BSearchTree<t>& bst) noexcept;
+
+    /*
+        Imports each element in iterable sturcture to this tree
+        *noexcept
+    */
     template<typename d>
-    BSearchTree(d& otherIterable);
-	void add(t value);
-    int remove(t value);
-    //merge method does not clear merged bst
-    void merge(BSearchTree<t>& bst);
+    BSearchTree(const d& otherIterable) noexcept;
+    
+    /*
+        Merges given structure with this tree
+        Does not clears the merged structure
+        *noexcept
+    */
+    void merge(BSearchTree<t>& bst) noexcept;
+
+    /*
+    Merges given structure with this tree
+    Does not clears the merged structure
+    *noexcept
+    */
     template<typename d>
-    void merge(d& otherIterable);
-    void clear();
-    bstNode<t>* search(t value);
-    bool isEmpty();
-    //forEach function does not allow elements to change
-    void forEach(std::function<void(t& element)> process);
-    ~BSearchTree();
+    void merge(const d& otherIterable) noexcept;
+    
+    /*
+        Visits all variables in order and executes the process function
+        Does not allow variables to change
+        *noexcept
+    */
+    void forEach(std::function<void(const t& element)> process)noexcept;
+
+    /*
+        Adds given value to tree
+        length value might overflow if there are more than 2^64 elements
+        *noexcept
+    */
+    void add(const t& value) noexcept;
+
+     /*
+        Adds given value to tree if value does not exists
+        length value might overflow if there are more than 2^64 elements
+        return true if succeed
+        *noexcept
+     */
+    bool addIfNotExists(const t& value) noexcept;
+
+    /*
+        Removes all elements
+        *noexcept
+    */
+    void clear() noexcept;  
+
+    /*
+        Removes the first node equal to value given
+        returns true if removing succeed
+        returns false if value is not found and removing failed
+        *noexcept
+    */
+    bool remove(const t& value) noexcept;
+
+    /*
+        Returns true if tree does not have any nodes
+        *noexcept *const
+    */
+    bool isEmpty() const noexcept;
+
+    /*
+        Returns pointer to node value searched
+        If value does not exist in tree returns nullptr
+        *noexcept *const
+    */
+    bstNode<t>* search(const t& value) const noexcept;
+
+    /*
+        Returns the number of nodes in the tree
+        *noexcept *const
+    */
+    size_t size() const noexcept;
+
+    /*
+        Removes all elements
+        *noexcept
+    */
+
+    void operator=(BSearchTree<t>& bst)noexcept;
+
+    ~BSearchTree() noexcept;
 };
 
 template<typename t>
-BSearchTree<t>::BSearchTree(std::initializer_list<t> initList) {
+BSearchTree<t>::BSearchTree(std::initializer_list<t> initList)noexcept {
     for (auto i : initList) {
         add(i);
     }
 }
 
 template<typename t>
-BSearchTree<t>::BSearchTree(BSearchTree<t>& bst) {
+BSearchTree<t>::BSearchTree(const BSearchTree<t>& bst)noexcept {
     bst.forEach([&](t element) {
         this->add(element);
                 });
@@ -58,14 +151,19 @@ BSearchTree<t>::BSearchTree(BSearchTree<t>& bst) {
 
 template<typename t>
 template<typename d>
-BSearchTree<t>::BSearchTree(d& otherIterable) {
-    for (auto i : other) {
+BSearchTree<t>::BSearchTree(const d& otherIterable) noexcept{
+    for (auto i : otherIterable) {
         this->add(i);
     }
 }
 
+template<typename t>
+size_t BSearchTree<t>::size() const noexcept {
+    return this->length;
+}
+
 template <typename t>
-void BSearchTree<t>::add(t value) {
+void BSearchTree<t>::add(const t& value) noexcept {
     if (start == nullptr) {
         bstNode<t>* newnode = new bstNode<t>();
         newnode->m_value = value;
@@ -99,11 +197,51 @@ void BSearchTree<t>::add(t value) {
         }
     }
     this->length += 1;
-
 }
 
 template <typename t>
-int BSearchTree<t>::remove(t value) {
+bool BSearchTree<t>::addIfNotExists(const t& value) noexcept {
+    if (start == nullptr) {
+        bstNode<t>* newnode = new bstNode<t>();
+        newnode->m_value = value;
+        start = newnode;
+    }
+    else {
+        auto currentNode{ start };
+        while (true) {
+            if (value > currentNode->m_value) {
+                if (currentNode->left == nullptr) {
+                    bstNode<t>* newnode = new bstNode<t>();
+                    newnode->m_value = value;
+                    currentNode->left = newnode;
+                    break;  // this concludes
+                }
+                else {
+                    currentNode = currentNode->left;
+                }
+            }
+            else if (value == currentNode->m_value) {
+                return false;
+            }
+            else {
+                if (currentNode->right == nullptr) {
+                    bstNode<t>* newnode = new bstNode<t>();
+                    newnode->m_value = value;
+                    currentNode->right = newnode;
+                    break;  // this concludes
+                }
+                else {
+                    currentNode = currentNode->right;
+                }
+            }
+        }
+    }
+    this->length += 1;
+    return true;
+}
+
+template <typename t>
+bool BSearchTree<t>::remove(const t& value) noexcept{
     auto currentNode{ start };
     bstNode<t>* beforeNode{};
     while (currentNode != nullptr) {
@@ -119,9 +257,11 @@ int BSearchTree<t>::remove(t value) {
             currentNode = currentNode->right;
         }
     }
+    // value not found
     if (currentNode == nullptr) {
-        return -1;
+        return false;
     }
+
     if (currentNode->left == nullptr && currentNode->right == nullptr) {
         if (beforeNode == nullptr) {
             delete start;
@@ -197,12 +337,12 @@ int BSearchTree<t>::remove(t value) {
         }
     }
     this->length -= 1;
-    return 0;
+    return true;
     
 }
 
 template <typename t>
-bstNode<t>* BSearchTree<t>::search(t value) {
+bstNode<t>* BSearchTree<t>::search(const t& value)const noexcept {
     auto currentNode{ start };
     while (currentNode != nullptr) {
         if (currentNode->m_value == value) {
@@ -256,7 +396,7 @@ void BSearchTree<t>::traverse(std::function<void(bstNode<t>* element)> process) 
 }
 
 template <typename t>
-void BSearchTree<t>::clear() {
+void BSearchTree<t>::clear() noexcept{
     traverse([](bstNode<t>* element) {
         delete element;
              });
@@ -265,33 +405,39 @@ void BSearchTree<t>::clear() {
 }
 
 template <typename t>
-bool BSearchTree<t>::isEmpty() {
+bool BSearchTree<t>::isEmpty() const noexcept {
     return start == nullptr;
 }
 
 template <typename t>
-void BSearchTree<t>::forEach(std::function<void(t& element)> process) {
-    traverse([&](bstNode<t>* elementptr) {
+void BSearchTree<t>::forEach(std::function<void(const t& element)> process) noexcept{
+    traverse([&process](bstNode<t>* elementptr) {
         process(elementptr->m_value);
              });
 }
 
 template <typename t>
-void BSearchTree<t>::merge(BSearchTree<t>& bst) {
-    bst.forEach([&](t element) {
+void BSearchTree<t>::merge(BSearchTree<t>& bst) noexcept {
+    bst.forEach([&](const t& element) {
         this->add(element);
                 });
 }
 
 template <typename t>
 template <typename d>
-void BSearchTree<t>::merge(d& otherIterable) {
+void BSearchTree<t>::merge(const d& otherIterable) noexcept  {
     for (auto i : otherIterable) {
         this->add(i);
     }
 }
 
 template <typename t>
-BSearchTree<t>::~BSearchTree() {
+void BSearchTree<t>::operator=(BSearchTree<t>& bst) noexcept{
+    this->clear();
+    this->merge(bst);
+}
+
+template <typename t>
+BSearchTree<t>::~BSearchTree() noexcept  {
     clear();
 }
